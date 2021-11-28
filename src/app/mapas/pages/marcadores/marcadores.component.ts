782b80ff19d89e1cea40be7351f1bab3ffb36cd2
Felix José Arteaga trigo <farteaga21791@gmail.com>
2021-11-28 20:43:49 +0100

    Fin sección 21

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,147 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorConColor {
+  color: string;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number];
+}
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+      .list-group {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        z-index: 99;
+      }
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-6.539760554468933, 38.72357049094892];
 
-  constructor() { }
+  // Array de marcadores:
+  marcadores: MarcadorConColor[] = [];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    this.leerMarcadorLocalStorage();
+
+    //Si queremos personalizar el marcador:
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Mi casa';
+
+    // new mapboxgl.Marker({
+    //   element: markerHtml,
+    // })
+    //   .setLngLat(this.center)
+    //   .addTo(this.mapa);
+
+    // Añadir un marcador:
+    // new mapboxgl.Marker().setLngLat(this.center).addTo(this.mapa);
   }
 
+  agregarMarcador() {
+    // Generar color aleatorio:
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const nuevoMarcador = new mapboxgl.Marker({ draggable: true, color: color })
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+
+    this.marcadores.push({
+      color: color,
+      marker: nuevoMarcador,
+    });
+    this.guardarMarcadoresLocalStorage();
+
+    nuevoMarcador.on('dragend', () => {
+      this.guardarMarcadoresLocalStorage();
+    });
+  }
+
+  irMarcador(marker: mapboxgl.Marker) {
+    this.mapa.flyTo({
+      center: marker.getLngLat(),
+      zoom: 18,
+    });
+  }
+
+  guardarMarcadoresLocalStorage() {
+    const lngLatArr: MarcadorConColor[] = [];
+
+    this.marcadores.forEach((m) => {
+      const color = m.color;
+      const { lng, lat } = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: color,
+        centro: [lng, lat],
+      });
+    });
+
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr));
+  }
+
+  leerMarcadorLocalStorage() {
+    if (!localStorage.getItem('marcadores')) {
+      return;
+    }
+    const lngLatArr: MarcadorConColor[] = JSON.parse(
+      localStorage.getItem('marcadores')!
+    );
+
+    lngLatArr.forEach((m) => {
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true,
+      })
+        .setLngLat(m.centro!)
+        .addTo(this.mapa);
+
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color,
+      });
+
+      newMarker.on('dragend', () => {
+        this.guardarMarcadoresLocalStorage();
+      });
+    });
+  }
+  borrarMarcador(i: number) {
+    this.marcadores[i].marker?.remove();
+    this.marcadores.splice(i, 1);
+    this.guardarMarcadoresLocalStorage();
+  }
 }
--- src/app/mapas/pages/propiedades/propiedades.component.ts
+++ src/app/mapas/pages/propiedades/propiedades.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Lugar {
+  titulo: string;
+  descripcion: string;
+  lngLat: [number, number];
+}
 
 @Component({
   selector: 'app-propiedades',
   templateUrl: './propiedades.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PropiedadesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PropiedadesComponent {
+  lugares: Lugar[] = [
+    {
+      titulo: 'Mi casa, Solana de los Barros',
+      descripcion: 'Badajoz-Extremadura-España',
+      lngLat: [-6.53951, 38.7237],
+    },
+    {
+      titulo: 'Campo de futbol, Solana de los Barros',
+      descripcion: 'Badajoz-Extremadura-España',
+      lngLat: [-6.53679, 38.72841],
+    },
+    {
+      titulo: 'Piscina Municipal, Solana de los Barros',
+      descripcion: 'Badajoz-Extremadura-España',
+      lngLat: [-6.53616, 38.73052],
+    },
+    {
+      titulo: 'Cooperativa Agrosola, Solana de los Barros',
+      descripcion: 'Badajoz-Extremadura-España',
+      lngLat: [-6.54165, 38.72207],
+    },
+  ];
 }
